62ec033460532c826a7b7efd43ce507f3fa51d25
kamer <freemaka@mail.gvsu.edu>
2020-11-11 16:14:15 -0500

    Graphing calc inputs

--- app/src/main/java/com/calculatorapp/GraphingCalcInputsActivity.java
+++ app/src/main/java/com/calculatorapp/GraphingCalcInputsActivity.java
@@ -10,9 +10,16 @@ import android.widget.EditText;
 
 import com.example.calculatorapp.R;
 
+import java.util.ArrayList;
+
 public class GraphingCalcInputsActivity extends AppCompatActivity {
-    public static double m;
-    public static double b;
+
+    //public static double m[] = new double[4];
+    //public static double b[] = new double[4];
+
+    public static ArrayList<Double> m = new ArrayList<>();
+    public static ArrayList<Double> b = new ArrayList<>();
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,47 +28,98 @@ public class GraphingCalcInputsActivity extends AppCompatActivity {
 
 
         Button graphButton = (Button) findViewById(R.id.graphButton);
+        Button extraEquation = (Button) findViewById(R.id.extraEquation);
         final EditText linearEquation = (EditText) findViewById(R.id.linearEquationEditText);
 
+
+        extraEquation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+
+                    //Get first equation
+                    String equation = linearEquation.getText().toString();
+                    int indOfX = equation.indexOf('x');
+                    if (Character.isDigit(equation.charAt(0)) || equation.charAt(0) == '.') {
+                        //m[i] = Double.parseDouble(equation.substring(0, indOfX));
+                        m.add(Double.parseDouble(equation.substring(0, indOfX)));
+                    }
+                    if (equation.charAt(0) == '-') {
+                        //m[i] = -(Double.parseDouble(equation.substring(1, indOfX)));
+                        m.add(-(Double.parseDouble(equation.substring(1, indOfX))));
+                    }
+
+
+                    int length = equation.length();
+                    if (equation.substring(indOfX).indexOf('+') == -1) {
+                        int mid = equation.substring(indOfX).indexOf('-') + indOfX;
+                        if (Character.isDigit(equation.charAt(mid + 1)) || equation.charAt(mid + 1) == '.') {
+                            //b[i] = -(Double.parseDouble(equation.substring(mid + 1)));
+                            b.add(-(Double.parseDouble(equation.substring(mid + 1))));
+                        } else {
+                            //b[i] = -(Double.parseDouble(equation.substring(mid + 2)));
+                            b.add(-(Double.parseDouble(equation.substring(mid + 2))));
+                        }
+                    } else {
+                        int mid = equation.substring(indOfX).indexOf('+') + indOfX;
+                        if (Character.isDigit(equation.charAt(mid + 1)) || equation.charAt(mid + 1) == '.') {
+                            //b[i] = Double.parseDouble(equation.substring(mid + 1));
+                            b.add(Double.parseDouble(equation.substring(mid + 1)));
+                        } else {
+                            //b[i] = Double.parseDouble(equation.substring(mid + 2));
+                            b.add(Double.parseDouble(equation.substring(mid + 2)));
+                        }
+                    }
+
+                    linearEquation.clearComposingText();
+                    //System.out.println("m[" + i + "] equals " + m[i] +  " b[" + i + "] equals " + b[i]);
+                    i++;
+                }
+
+        });
+
+        //When graph button is clicked
         graphButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
 
-
-                String equation = linearEquation.getText().toString();
+                //Get first equation
+               /* String equation = linearEquation.getText().toString();
                 int indOfX = equation.indexOf('x');
                 if (Character.isDigit(equation.charAt(0)) || equation.charAt(0) == '.'){
-                    m = Double.parseDouble(equation.substring(0,indOfX));
+                    m[0] = Double.parseDouble(equation.substring(0,indOfX));
                 }
                 if (equation.charAt(0) == '-'){
-                    m = -(Double.parseDouble(equation.substring(1,indOfX)));
+                    m[0] = -(Double.parseDouble(equation.substring(1,indOfX)));
                 }
-
 
 
                 int length = equation.length();
-
                 if (equation.substring(indOfX).indexOf('+') == -1){
                     int mid = equation.substring(indOfX).indexOf('-') + indOfX;
                     if (Character.isDigit(equation.charAt(mid +1)) || equation.charAt(mid + 1) == '.'){
-                        b = -(Double.parseDouble(equation.substring(mid+1)));
+                        b[0] = -(Double.parseDouble(equation.substring(mid+1)));
                     }else{
-                        b = -(Double.parseDouble(equation.substring(mid+2)));
+                        b[0] = -(Double.parseDouble(equation.substring(mid+2)));
                     }
                 }else{
                     int mid = equation.substring(indOfX).indexOf('+') + indOfX;
                     if (Character.isDigit(equation.charAt(mid+1)) || equation.charAt(mid+1) == '.'){
-                        b = Double.parseDouble(equation.substring(mid+1));
+                        b[0] = Double.parseDouble(equation.substring(mid+1));
                     }else{
-                        b = Double.parseDouble(equation.substring(mid+2));
+                        b[0] = Double.parseDouble(equation.substring(mid+2));
                     }
                 }
 
+                */
 
-                //b = Integer.parseInt(linearEquation.getText().toString().substring(length - 1));
 
+                //for (int j = 0; j < 4; j++){
+                  //  System.out.println("m[" + j + "] = " + m[j] + "b[" + j + "] = " + b[j]);
+                //}
 
+                //Displays graph
                 Intent graphingCalculatorScreen = new Intent(GraphingCalcInputsActivity.this,
                         graphingActivity.class);
                 startActivity(graphingCalculatorScreen);
--- app/src/main/java/com/calculatorapp/graphingActivity.java
+++ app/src/main/java/com/calculatorapp/graphingActivity.java
@@ -2,8 +2,13 @@ package com.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 
 import com.example.calculatorapp.R;
 import com.jjoe64.graphview.GraphView;
@@ -13,8 +18,12 @@ import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class graphingActivity extends AppCompatActivity {
-    private double m = GraphingCalcInputsActivity.m;
-    private double b = GraphingCalcInputsActivity.b;
+    //private double m[] = GraphingCalcInputsActivity.m;
+    //private double b[] = GraphingCalcInputsActivity.b;
+
+    private ArrayList<Double> m = GraphingCalcInputsActivity.m;
+    private ArrayList<Double> b = GraphingCalcInputsActivity.b;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,17 +32,61 @@ public class graphingActivity extends AppCompatActivity {
 
 
         GraphView graph = (GraphView) findViewById(R.id.graph);
-        LineGraphSeries<DataPoint> dataset = new LineGraphSeries<>();
-
+        ArrayList<LineGraphSeries<DataPoint>> dataset = new ArrayList<>();
+        ////LineGraphSeries<DataPoint> dataset2 = new LineGraphSeries<>();
+        LineGraphSeries<DataPoint> dataset3 = new LineGraphSeries<>();
+        //LineGraphSeries<DataPoint> dataset4 = new LineGraphSeries<>();
 
         double y;
 
+        for (int i =0; i< m.size(); i++) {
+            LineGraphSeries<DataPoint> dataset1 = new LineGraphSeries<>();
+            for (int x = -90; x < 90; x++) {
+                //y = (m[i] * x) + b[i];
+                y = (m.get(i) * x) + b.get(i);
+                dataset1.appendData(new DataPoint(x,y),true,180);
+            }
+            dataset.add(dataset1);
+        }
+
         // if linear equation
-        for (int x = -90; x <90; x++){
-            y = (m * x) + b;
+        /*for (int x = -90; x <90; x++){
+            y = (m[0] * x) + b[0];
             dataset.appendData(new DataPoint(x,y),true,180);
         }
+
+        for (int x = -90; x <90; x++){
+            y = (m[1] * x) + b[1];
+            dataset2.appendData(new DataPoint(x,y),true,180);
+        }
+
+        for (int x = -90; x <90; x++){
+            y = (m[2] * x) + b[2];
+            dataset3.appendData(new DataPoint(x,y),true,180);
+        }
+
+        for (int x = -90; x <90; x++){
+            y = (m[3] * x) + b[3];
+            dataset4.appendData(new DataPoint(x,y),true,180);
+        }
+
         graph.addSeries(dataset);
+        graph.addSeries(dataset2);
+        graph.addSeries(dataset3);
+        graph.addSeries(dataset4);
+
+         */
+
+        for (int i =0; i < dataset.size(); i++){
+            graph.addSeries(dataset.get(i));
+            dataset.get(i).setTitle("Equation " + (i+1) );
+            dataset.get(i).setThickness(8);
+        }
+
+        //dataset.get(0).setColor(Color.RED);
+        //dataset.get(1).setColor(Color.BLUE);
+        //dataset.get(2).setColor(Color.BLACK);
+        //dataset.get(3).setColor(Color.YELLOW);
 
         graph.getViewport().setMinX(-10);
         graph.getViewport().setMaxX(10);
@@ -54,11 +107,14 @@ public class graphingActivity extends AppCompatActivity {
 
         //set color, title of curve, DataPoints Radius, thickness
 
-        dataset.setColor(Color.RED);
-        dataset.setTitle("Data Set");
+        //dataset.setColor(Color.RED);
+        //dataset.setTitle("Data Set");
         //dataset.setDrawDataPoints(true);
         //dataset.setDataPointsRadius(16);
-        dataset.setThickness(8);
+        //dataset.setThickness(8);
+        //dataset2.setColor(Color.BLUE);
+        //dataset3.setColor(Color.BLACK);
+        //dataset4.setColor(Color.YELLOW);
 
         //set Title of graph
         graph.setTitle("Graph");
@@ -78,4 +134,14 @@ public class graphingActivity extends AppCompatActivity {
 
 
     }
+
+    @Override
+    public void onBackPressed(){
+        m.clear();
+        b.clear();
+        Intent intent = new Intent(this, GraphingCalcInputsActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
 }
