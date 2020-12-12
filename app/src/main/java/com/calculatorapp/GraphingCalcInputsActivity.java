a7cd7f0e57d6c1962dc88cf74265863215634823
kamer <freemaka@mail.gvsu.edu>
2020-12-12 17:06:07 -0500

    Added trig functions to graphing calc plus fixed a bunch of small problems regarding format and made try/catch exception so app doesnt crash if calc cant graph function

--- app/src/main/java/com/calculatorapp/GraphingCalcInputsActivity.java
+++ app/src/main/java/com/calculatorapp/GraphingCalcInputsActivity.java
@@ -2,19 +2,27 @@ package com.calculatorapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.calculatorapp.R;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class GraphingCalcInputsActivity extends AppCompatActivity {
+
     public static ArrayList<Double> m = new ArrayList<>();
     public static ArrayList<Double> b = new ArrayList<>();
+    public static ArrayList<String> trig = new ArrayList<>();
+    public static ArrayList<Double> trigb = new ArrayList<>();
     int i = 0;
 
     @Override
@@ -23,53 +31,109 @@ public class GraphingCalcInputsActivity extends AppCompatActivity {
         setContentView(R.layout.graphing_calc_inputs);
 
 
-        Button graphButton = (Button) findViewById(R.id.graphButton);
-        Button extraEquation = (Button) findViewById(R.id.extraEquation);
-        final EditText linearEquation = (EditText) findViewById(R.id.linearEquationEditText);
+        Button graphButton = findViewById(R.id.graphButton);
+        Button extraEquation = findViewById(R.id.extraEquation);
+        final EditText linearEquation = findViewById(R.id.linearEquationEditText);
 
 
         extraEquation.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                try {
+                    int trigornot = 1;
 
                     //Get first equation
                     String equation = linearEquation.getText().toString();
                     int indOfX = equation.indexOf('x');
-                    if (Character.isDigit(equation.charAt(0)) || equation.charAt(0) == '.') {
-                        //m[i] = Double.parseDouble(equation.substring(0, indOfX));
-                        m.add(Double.parseDouble(equation.substring(0, indOfX)));
-                    }
-                    if (equation.charAt(0) == '-') {
-                        //m[i] = -(Double.parseDouble(equation.substring(1, indOfX)));
-                        m.add(-(Double.parseDouble(equation.substring(1, indOfX))));
+                    if (equation.contains("sin") || equation.contains("Sin")) {
+                        trig.add(equation.substring(0, indOfX + 2));
+                        trigornot = 2;
+                    } else if (equation.contains("cos") || equation.contains("Cos")) {
+                        trig.add(equation.substring(0, indOfX + 2));
+                        trigornot = 2;
+                    } else if (equation.contains("tan") || equation.contains("Tan")) {
+                        trig.add(equation.substring(0, indOfX + 2));
+                        trigornot = 2;
+                    } else {
+
+                        if (Character.isDigit(equation.charAt(0)) || equation.charAt(0) == '.') {
+                            //m[i] = Double.parseDouble(equation.substring(0, indOfX));
+                            m.add(Double.parseDouble(equation.substring(0, indOfX)));
+                        }
+                        if (equation.charAt(0) == '-') {
+                            //m[i] = -(Double.parseDouble(equation.substring(1, indOfX)));
+                            if (equation.charAt(1) == 'x')
+                                m.add(-1.0);
+                            else
+                                m.add(-(Double.parseDouble(equation.substring(1, indOfX))));
+
+                        }
+                        if (equation.charAt(0) == 'x') {
+                            m.add(1.0);
+                        }
                     }
 
 
                     int length = equation.length();
-                    if (equation.substring(indOfX).indexOf('+') == -1) {
-                        int mid = equation.substring(indOfX).indexOf('-') + indOfX;
-                        if (Character.isDigit(equation.charAt(mid + 1)) || equation.charAt(mid + 1) == '.') {
-                            //b[i] = -(Double.parseDouble(equation.substring(mid + 1)));
-                            b.add(-(Double.parseDouble(equation.substring(mid + 1))));
+                    if (equation.substring(indOfX).indexOf('+') == -1 && equation.substring(indOfX).indexOf('-') == -1) {
+                        if (trigornot == 2) {
+                            trigb.add(0.0);
                         } else {
-                            //b[i] = -(Double.parseDouble(equation.substring(mid + 2)));
-                            b.add(-(Double.parseDouble(equation.substring(mid + 2))));
+                            b.add(0.0);
                         }
                     } else {
-                        int mid = equation.substring(indOfX).indexOf('+') + indOfX;
-                        if (Character.isDigit(equation.charAt(mid + 1)) || equation.charAt(mid + 1) == '.') {
-                            //b[i] = Double.parseDouble(equation.substring(mid + 1));
-                            b.add(Double.parseDouble(equation.substring(mid + 1)));
+                        if (equation.substring(indOfX).indexOf('+') == -1) {
+                            int mid = equation.substring(indOfX).indexOf('-') + indOfX;
+                            if (Character.isDigit(equation.charAt(mid + 1)) || equation.charAt(mid + 1) == '.') {
+                                //b[i] = -(Double.parseDouble(equation.substring(mid + 1)));
+                                if (trigornot == 2) {
+                                    trigb.add(-(Double.parseDouble(equation.substring(mid + 1))));
+                                } else {
+                                    b.add(-(Double.parseDouble(equation.substring(mid + 1))));
+                                }
+                            } else {
+                                //b[i] = -(Double.parseDouble(equation.substring(mid + 2)));
+                                if (trigornot == 2) {
+                                    trigb.add(-(Double.parseDouble(equation.substring(mid + 2))));
+                                } else {
+                                    b.add(-(Double.parseDouble(equation.substring(mid + 2))));
+                                }
+                            }
                         } else {
-                            //b[i] = Double.parseDouble(equation.substring(mid + 2));
-                            b.add(Double.parseDouble(equation.substring(mid + 2)));
+                            int mid = equation.substring(indOfX).indexOf('+') + indOfX;
+                            if (Character.isDigit(equation.charAt(mid + 1)) || equation.charAt(mid + 1) == '.') {
+                                //b[i] = Double.parseDouble(equation.substring(mid + 1));
+                                if (trigornot == 2) {
+                                    trigb.add(Double.parseDouble(equation.substring(mid + 1)));
+                                } else {
+                                    b.add(Double.parseDouble(equation.substring(mid + 1)));
+                                }
+                            } else {
+                                //b[i] = Double.parseDouble(equation.substring(mid + 2));
+                                if (trigornot == 2) {
+                                    trigb.add(Double.parseDouble(equation.substring(mid + 2)));
+                                } else {
+                                    b.add(Double.parseDouble(equation.substring(mid + 2)));
+                                }
+                            }
                         }
                     }
 
-                    linearEquation.clearComposingText();
 
+                    linearEquation.clearComposingText();
+                }catch(Exception e){
+                    //Snackbar.make(findViewById(R.id.extraEquation), "INVALID INPUT/FORMAT", Snackbar.LENGTH_LONG).show().;
+                    Toast toast = Toast.makeText(GraphingCalcInputsActivity.this, "INVALID INPUT/FORMAT\nClearing functions", Toast.LENGTH_LONG);
+                    toast.setGravity(Gravity.CENTER,0,0);
+                    toast.show();
+                    m.clear();
+                    b.clear();
+                    trig.clear();
+                    trigb.clear();
+                    //AlertDialogtrig.Builder builer = new AlertDialog.Builder(AlertDialogActivity.this).create();
                 }
+            }
+
 
         });
 
--- app/src/main/java/com/calculatorapp/graphingActivity.java
+++ app/src/main/java/com/calculatorapp/graphingActivity.java
@@ -26,6 +26,8 @@ import com.jjoe64.graphview.series.Series;
 public class graphingActivity extends AppCompatActivity {
     private ArrayList<Double> m = GraphingCalcInputsActivity.m;
     private ArrayList<Double> b = GraphingCalcInputsActivity.b;
+    private ArrayList<String> trig = GraphingCalcInputsActivity.trig;
+    private ArrayList<Double> trigb = GraphingCalcInputsActivity.trigb;
 
 
     @Override
@@ -34,21 +36,69 @@ public class graphingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_graphing);
 
 
-        GraphView graph = (GraphView) findViewById(R.id.graph);
+        GraphView graph = findViewById(R.id.graph);
         ArrayList<LineGraphSeries<DataPoint>> dataset = new ArrayList<>();
 
 
         double y;
 
         // creates datasets and adds them to dataset arraylist
-        for (int i =0; i< m.size(); i++) {
+
+
+
+        for (int i =0; i< m.size() || i < trig.size(); i++) {
             LineGraphSeries<DataPoint> dataset1 = new LineGraphSeries<>();
-            for (int x = -90; x < 90; x++) {
-                y = (m.get(i) * x) + b.get(i);
-                dataset1.appendData(new DataPoint(x,y),true,180);
+            if (i < m.size()) {
+                for (int x = -90; x < 90; x++) {
+                    y = (m.get(i) * x) + b.get(i);
+                    dataset1.appendData(new DataPoint(x, y), true, 180);
+                }
+                dataset.add(dataset1);
             }
-            dataset.add(dataset1);
+            if (i < trig.size()) {
+                LineGraphSeries<DataPoint> dataset2 = new LineGraphSeries<>();
+                double x = -90.0;
+                double scalar;
+                int indOfP = trig.get(i).indexOf('(') + 1;
+                int indOfX = trig.get(i).indexOf('x');
+                if (indOfP == indOfX)
+                    scalar = 1.0;
+                else if (trig.get(i).charAt(indOfP) == '-' && (indOfP +1) == indOfX)
+                    scalar = -1.0;
+                else
+                    scalar = Double.parseDouble(trig.get(i).substring(indOfP,indOfX));
+                for (int l = 0; l < 5000; l++) {
+                    x = x + .1;
+                    if (trig.get(i).contains("sin") || trig.get(i).contains("Sin")) {
+                        if(trig.get(i).charAt(0) == '-'){
+                            y = (-Math.sin(scalar * x)) + trigb.get(i);
+                            dataset2.appendData(new DataPoint(x, y), true, 5000);
+                        }else {
+                            y = Math.sin(scalar * x) + trigb.get(i);
+                            dataset2.appendData(new DataPoint(x, y), true, 5000);
+                        }
+                    }else if (trig.get(i).contains("cos") || trig.get(i).contains("Cos")){
+                        if(trig.get(i).charAt(0) == '-'){
+                            y = (-Math.cos(scalar * x)) + trigb.get(i);
+                            dataset2.appendData(new DataPoint(x, y), true, 5000);
+                        }else {
+                            y = Math.cos(scalar * x) + trigb.get(i);
+                            dataset2.appendData(new DataPoint(x, y), true, 5000);
+                        }
+                    }else if (trig.get(i).contains("tan") || trig.get(i).contains("Tan")){
+                        if(trig.get(i).charAt(0) == '-'){
+                            y = (-Math.tan(scalar * x)) + trigb.get(i);
+                            dataset2.appendData(new DataPoint(x, y), true, 5000);
+                        }else {
+                            y = Math.tan(scalar * x) + trigb.get(i);
+                            dataset2.appendData(new DataPoint(x, y), true, 5000);
+                        }
+                    }
+                }
+                dataset.add(dataset2);
+            }
         }
+
 
         // array to hold colors
         int[] color = {Color.RED, Color.BLACK, Color.BLUE, Color.YELLOW, Color.GREEN, Color.CYAN};
@@ -59,6 +109,9 @@ public class graphingActivity extends AppCompatActivity {
             graph.addSeries(dataset.get(i));
             dataset.get(i).setTitle("Equation " + (i+1) );
             dataset.get(i).setThickness(8);
+            dataset.get(i).isDrawAsPath();
+            dataset.get(i).setDrawAsPath(true);
+            //dataset.get(i).setDrawDataPoints(true);
             if (c > color.length - 1){
                 c=0;
             }
@@ -80,6 +133,10 @@ public class graphingActivity extends AppCompatActivity {
         graph.getViewport().setMaxX(10);
         graph.getViewport().setMinY(-10);
         graph.getViewport().setMaxY(10);
+        graph.getViewport().setYAxisBoundsManual(true);
+        graph.getViewport().setXAxisBoundsManual(true);
+
+
 
         //allows zooming
         graph.getViewport().setScalable(true);
